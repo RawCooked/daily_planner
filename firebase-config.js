5efe40f9c1ed6f3c1e01eee89738288e258e8ba3
RawCooked <123646029+RawCooked@users.noreply.github.com>
2025-11-14 22:07:35 +0100

    Add Firebase configuration and initialization

---
+++ firebase-config.js
@@ -0,0 +1,16 @@
+// Firebase Configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCL4cB6loL0XtxljIUa8Hd92Ff4mw9ftO8",
+    authDomain: "eshopproject-19bb4.firebaseapp.com",
+    projectId: "eshopproject-19bb4",
+    storageBucket: "eshopproject-19bb4.appspot.com",
+    messagingSenderId: "863268336252",
+    appId: "1:863268336252:web:e90c30527cf0c56f0d4ff9"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+
+// Password (Change this to your desired password)
+const PLANNER_PASSWORD = "221Jmt4377";
